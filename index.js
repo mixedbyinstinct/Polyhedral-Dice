d0829b3310d9eedf70f09a4902863abba2ae3b0e
mixedbyinstinct <92763310+mixedbyinstinct@users.noreply.github.com>
2022-05-29 20:17:04 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 //list of numbers to generate roll from
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-//counter to stop recursion
-var c = 0;
-
 //variables to store final roll and index in number list
 var stop;
 var index;
+var stopped = false;
 
 document.getElementById('d4').onclick = function side4() {
   //get random index from a range equal to the number of sides on the die
@@ -15,92 +13,89 @@ document.getElementById('d4').onclick = function side4() {
   //set stop = to the number at the aforementioned index in the list
   stop = numbers[index];
 
-  //stop recursion when counter hits 20
-  if(c === 20) {
+  //stop recursion when user hits stop button
+  if(stopped) {
     document.getElementById("rolled")
    .innerHTML = " rolled: " + stop;
-    c = 0;
+    stopped = false;
     return;
   }
   document.getElementById("rolled")
   .innerHTML = stop;
-  //increment counter post recursion
-  c++;
   //recurse
   setTimeout(function(){side4()}, 100);
+}
+
+document.getElementById("stop").onclick = function() {
+  stopped = true;
 }
 
 document.getElementById('d6').onclick = function side6() {
   index = Math.floor(Math.random() * 6);
   stop = numbers[index];
-  if(c === 20) {
+  if(stopped) {
     document.getElementById("rolled")
    .innerHTML = " rolled: " + stop;
-    c = 0;
+    stopped = false;
     return;
   }
   document.getElementById("rolled")
   .innerHTML = stop;
-  c++;
   setTimeout(function(){side6()}, 100);
 }
 
 document.getElementById('d8').onclick = function side8() {
   index = Math.floor(Math.random() * 8);
   stop = numbers[index];
-  if(c === 20) {
+  if(stopped) {
     document.getElementById("rolled")
    .innerHTML = " rolled: " + stop;
-    c = 0;
+    stopped = false;
     return;
   }
   document.getElementById("rolled")
   .innerHTML = stop;
-  c++;
   setTimeout(function(){side8()}, 100);
 }
 
 document.getElementById('d10').onclick = function side10() {
   index = Math.floor(Math.random() * 10);
   stop = numbers[index];
-  if(c === 20) {
+  if(stopped) {
     document.getElementById("rolled")
    .innerHTML = " rolled: " + stop;
-    c = 0;
+    stopped = false;
     return;
   }
   document.getElementById("rolled")
   .innerHTML = stop;
-  c++;
   setTimeout(function(){side10()}, 100);
 }
 
 document.getElementById('d12').onclick = function side12() {
   index = Math.floor(Math.random() * 12);
   stop = numbers[index];
-  if(c === 20) {
+  if(stopped) {
     document.getElementById("rolled")
    .innerHTML = " rolled: " + stop;
-    c = 0;
+    stopped = false;
     return;
   }
   document.getElementById("rolled")
   .innerHTML = stop;
-  c++;
   setTimeout(function(){side12()}, 100);
 }
 
 document.getElementById('d20').onclick = function side20() {
   index = Math.floor(Math.random() * 20);
   stop = numbers[index];
-  if(c === 20) {
+  if(stopped) {
     document.getElementById("rolled")
    .innerHTML = " rolled: " + stop;
-    c = 0;
+    stopped = false;
     return;
   }
   document.getElementById("rolled")
   .innerHTML = stop;
-  c++;
   setTimeout(function(){side20()}, 100);
 }
